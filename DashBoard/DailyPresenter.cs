4f212039b20805f3747dc7e6c4bf0a4740d05906
Eduard <eduardbargues@gmail.com>
2017-12-21 12:37:41 +0100

    - first commit

---
+++ DashBoard/DailyPresenter.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TeslaAnalysis;
+using TimeSeriesAnalysis;
+
+namespace DashBoard
+{
+    public interface IDailyView
+    {
+        IStatisticsView GetStatisticsView();
+        DateTime? SelectedDay { get; }
+        event Action RefreshRequest;
+        bool ComputeCandlesByDuration { get; }
+        bool ComputeCandlesByTicks { get; }
+        bool ComputeCandlesByVolume { get; }
+        TimeSpan CandlesDuration { get; }
+        int CandlesTicks { get; }
+        double CandlesVolume { get; }
+        void LoadData(CandleTimeSeries series, IEnumerable<(TimeSeries, Color)> indicators);
+        void LoadDays(IEnumerable<DateTime> days);
+        bool DaysLoaded { get; }
+        int Period { get; }
+        int SmoothingPeriod { get; }
+    }
+
+    public class DailyPresenter
+    {
+        private readonly IDailyView view;
+        private readonly StatisticsPresenter statisticsPresenter;
+
+        public DailyPresenter(IDailyView view)
+        {
+            this.view = view;
+            statisticsPresenter = new StatisticsPresenter(view.GetStatisticsView());
+            this.view.RefreshRequest += View_RefreshRequest;
+        }
+
+        public void LoadDays()
+        {
+            if (!view.DaysLoaded)
+            {
+                IEnumerable<DateTime> days = Context.Instance.HistoryCandleTimeSeries.Candles
+                    .Select(candle => candle.Start.Date)
+                    .Distinct();
+                view.LoadDays(days);
+            }
+        }
+
+        private void View_RefreshRequest()
+        {
+            LoadData();
+        }
+
+        public void LoadData()
+        {
+            if (view.SelectedDay != null)
+            {
+                DateTime day = view.SelectedDay.Value.Date;
+                List<Trade> dayBeforeTrades = Context.Instance.GetDailyTrades(day.AddDays(-1))
+                    .ToList();
+                List<Trade> dayTrades = Context.Instance.GetDailyTrades(day)
+                    .ToList();
+                (CandleTimeSeries daySeries, CandleTimeSeries twoDaysSeries) = CandleTimeSeries(dayTrades, day, dayBeforeTrades);
+                statisticsPresenter.LoadData(daySeries);
+                IEnumerable<(TimeSeries, Color)> indicators = UtilsPresenter.GetIndicators(twoDaysSeries, view.Period, view.SmoothingPeriod, day.Date);
+                view.LoadData(daySeries, indicators);
+            }
+        }
+
+        private (CandleTimeSeries, CandleTimeSeries) CandleTimeSeries(List<Trade> dayTrades, DateTime day, List<Trade> dayBeforeTrades)
+        {
+            CandleTimeSeries daySeries;
+            CandleTimeSeries twoDaysSeries;
+            if (view.ComputeCandlesByDuration)
+            {
+                TimeSpan candleDuration = view.CandlesDuration;
+                daySeries = GetCandleTimeSeriesByDuration(dayTrades, candleDuration, day.Date);
+                CandleTimeSeries dayBeforeSeries = GetCandleTimeSeriesByDuration(dayBeforeTrades, candleDuration, day.Date.AddDays(-1));
+                twoDaysSeries = dayBeforeSeries.Candles
+                    .Union(daySeries.Candles)
+                    .ToCandleTimeSeries();
+            }
+            else if (view.ComputeCandlesByTicks)
+            {
+                // TODO
+                throw new NotImplementedException();
+            }
+            else if (view.ComputeCandlesByVolume)
+            {
+                // TODO
+                throw new NotImplementedException();
+            }
+            else
+                throw new NotImplementedException();
+            return (daySeries, twoDaysSeries);
+        }
+
+        private CandleTimeSeries GetCandleTimeSeriesByDuration(IEnumerable<Trade> trades, TimeSpan candleDuration, DateTime firstCandleStart)
+        {
+
+            return trades
+                .ToCandlesByDuration(candleDuration, firstCandleStart)
+                .ToCandleTimeSeries($"{firstCandleStart:yyyy/MM/dd} - By duration");
+        }
+    }
+}
---
+++ DashBoard/UtilsPresenter.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TeslaAnalysis;
+using TeslaAnalysis.Indicators;
+using TimeSeriesAnalysis;
+
+namespace DashBoard
+{
+    public static class UtilsPresenter
+    {
+        public static IEnumerable<(TimeSeries, Color)> GetIndicators(CandleTimeSeries series
+            , int indicatorPeriod
+            , int smoothingPeriod
+            , DateTime startDate)
+        {
+            DirectionalIndicatorPlus diPlus =
+                DirectionalIndicatorPlus.Create(indicatorPeriod, smoothingPeriod);
+            TimeSeries diPlusSeries = series.Candles
+                .Where((candle, index) => index > 0 &&
+                                          candle.Start >= startDate)
+                .Select(candle => new DateValue(candle.Start, diPlus[series, candle.Start]))
+                .ToTimeSeries("DI+");
+            yield return (diPlusSeries, Color.Blue);
+
+            DirectionalIndicatorMinus diMinus =
+                DirectionalIndicatorMinus.Create(indicatorPeriod, smoothingPeriod);
+            TimeSeries diMinusSeries = series.Candles
+                .Where((candle, index) => index > 0 &&
+                                          candle.Start >= startDate)
+                .Select(candle => new DateValue(candle.Start, diMinus[series, candle.Start]))
+                .ToTimeSeries("DI-");
+            yield return (diMinusSeries, Color.Red);
+
+            DirectionalMovementIndex dx = DirectionalMovementIndex.Create(indicatorPeriod, smoothingPeriod);
+            TimeSeries dxSeries = series.Candles
+                .Where((candle, index) => index > 0 &&
+                                          candle.Start >= startDate)
+                .Select(candle => new DateValue(candle.Start, dx[series, candle.Start]))
+                .ToTimeSeries("DX");
+            yield return (dxSeries, Color.DarkSlateGray);
+
+            AverageDirectionalMovementIndex adx =
+                AverageDirectionalMovementIndex.Create(indicatorPeriod, smoothingPeriod);
+            TimeSeries adxSeries = series.Candles
+                .Where((candle, index) => index > 0 &&
+                                          candle.Start >= startDate)
+                .Select(candle => new DateValue(candle.Start, adx[series, candle.Start]))
+                .ToTimeSeries("ADX");
+            yield return (adxSeries, Color.DarkGray);
+        }
+    }
+}
